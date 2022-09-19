4427fa11f43f041cb9c9915963ed9a6ff189e6b6
Your Name <you@example.com>
2022-09-19 21:36:21 +0300

    first working ver

--- src/background.js
+++ src/background.js
@@ -1,21 +1,80 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+// let jobTitle = "initial bg injected job title"
+// let companyName = "initial bg injected company name"
+
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "@firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  setDoc
+} from "firebase/firestore";// Follow this pattern to import other Firebase services
+
+// TODO company account+ where to keep secret
+const firebaseConfig = {
+  apiKey: "AIzaSyBZAyI_ON7F4VEZ_9k1ETzF4_k6qcBJ1uo",
+  authDomain: "yuveta2-166505.firebaseapp.com",
+  databaseURL: "https://yuveta2-166505-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "yuveta2-166505",
+  storageBucket: "yuveta2-166505.appspot.com",
+  messagingSenderId: "238877844764",
+  appId: "1:238877844764:web:0b49af2563aa55139806d9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const jobsCollectionRef = collection(db, "jobs");
+
+
+
+const addJob = async (job) => {
+  const id = String(Math.floor(Math.random() * 10000) + 1);
+  const newJob = { id, ...job }
+  await setDoc(doc(db, "jobs", newJob.id), newJob);
+}
+
+
+chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
+
+    if (message.from == "Li-content-script"){
+      console.log("job name ", message.jobTitle, "company ", message.companyName)
+
+      if(message.type =="ADD"){
+
+
+        console.log("got ADD message from Li");
+
+       //payload = JSON.stringify({jobTitle: message.jobTitle, companyName: message.companyName});
+        addJob({jobTitle: message.jobTitle, companyName: message.companyName})
+
+      } else if (message.type =="QUERY") {
+
+
+        console.log("got QUERY message from Li");
+
+
+
+
+        const q = query(jobsCollectionRef, where("jobTitle", "==", message.jobTitle), where("companyName", "==", message.companyName));
+
+        getDocs(q).then((querySnapshot) => {
+          if(querySnapshot.size >0) {
+            console.log("job name ", message.jobTitle, "company ", message.companyName, "-> found in DB");
+            sendResponse({from: "bg script", type: "QUERY_RESP", jobfound: true});
+          } else {
+            console.log("job name ", message.jobTitle, "company ", message.companyName, "-> NOT found in DB");
+            sendResponse({from: "bg script", type: "QUERY_RESP", jobfound: false});
+          }
+        });
+      }
+    }
+    return true;
+
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,105 @@
-'use strict';
+console.log("Li yuval invoked!")
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+// maybe click is not the right time
+document.addEventListener("click", detectJobs);
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+let opened = false;
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
 
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
+var yuvDiv = document.createElement('div');
+yuvDiv.id = 'yuvDiv';
+yuvDiv.innerHTML = `
 
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
+<form id="yuvForm" class='add-form-hidden'>
+
+    <div class='form-control'>
+        <label>Company</label>
+        <input id="companyName" name="companyName" type='text' placeholder='Company name' />
+    </div>
+
+    <div class='form-control' >
+        <label>Job title</label>
+        <input id='jobTitle' name="jobTitle" type='text' placeholder='Job title' />
+    </div>
+
+    <div class='form-control'>
+        <input type='submit' value='Save Job' class='btn btn-block' />
+    </div>
+</form>
+
+<button id="drawerButton" type="submit"></button>
+
+
+`
+document.getElementsByTagName('body')[0].appendChild(yuvDiv);
+
+
+
+
+
+const form = document.getElementById('yuvForm');
+
+const drawerButton = document.getElementById('drawerButton');
+drawerButton.onclick = function () {
+  if (opened) {
+    form.className = "add-form-hidden";
+  } else {
+    form.className = "add-form";
   }
-);
+  opened = !opened;
+};
 
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
+form.addEventListener('submit', function (e) {
+  // Prevent default behavior:
+  e.preventDefault();
+
+  // Create payload as new FormData object:
+  const formData = new FormData(form);
+  payload = Object.fromEntries(formData);
+
+  chrome.runtime.sendMessage({ from: "Li-content-script", type: "ADD", ...payload });
+})
+
+
+
+let current_displayed_job = null;
+
+function detectJobs() {
+
+  //this might fire a lot,
+  //maybe avoid sending to backend if found in some local cache
+  // Array.from(document.getElementsByClassName("jobs-unified-top-card")[0].children).filter(v => v)[0]
+  if (document.getElementsByClassName("jobs-unified-top-card").length > 0) {
+
+    console.log("jobs-unified-top-card found!")
+
+    jobTitle = document.getElementsByClassName('jobs-unified-top-card__job-title')[0].innerHTML.replace(/^[^a-zA-Z0-9]*|[^a-zA-Z0-9]*$/g, '');
+    companyName = document.getElementsByClassName('jobs-unified-top-card__company-name')[0].children[0].innerHTML.replace(/^[^a-zA-Z0-9]*|[^a-zA-Z0-9]*$/g, '');
+
+    let new_job = companyName + jobTitle;
+
+    if (new_job != current_displayed_job) {
+
+      console.log("job name ", jobTitle, "company ", companyName)
+
+      // TODO slow as causes: contentscript - > background -> firestore 
+      // better have local cache in mem/local storage, only set of "unique" jobTitle+companyName 
+      chrome.runtime.sendMessage({ from: "Li-content-script", type: "QUERY", jobTitle: jobTitle, companyName: companyName }, function (response) {
+
+        console.log("got response from bg script:", response);
+        if (response.jobfound) {
+          console.log("job name ", jobTitle, "company ", companyName, " - > found, ignore")
+        } else {
+          console.log("job name ", jobTitle, "company ", companyName, " - > NOT found, suggest add")
+
+          document.getElementById("jobTitle").placeholder = jobTitle
+          document.getElementById("companyName").placeholder = companyName
+
+          document.getElementById("jobTitle").value = jobTitle
+          document.getElementById("companyName").value = companyName
+        }
+      });
+    }
+    current_displayed_job = new_job;
   }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
+}
